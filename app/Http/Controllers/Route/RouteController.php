46340bf1cdc6c12602f26e4b63b9c1480dd01942
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2020-05-27 13:36:35 +0100

    Add a controller for properly handling routes

---
+++ app/Http/Controllers/Route/RouteController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Route;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class RouteController extends Controller
+{
+    public function notFound(Request $request) {
+        return response()->json([
+            'message' => 'The given API route was not found',
+            'is_secure' => $request->secure(),
+            'user_agent' => $request->userAgent(),
+            'timestamp' => Carbon::now(),
+            'path' => $request->fullUrl(),
+            'method' => $request->method()
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    public function teapot(Request $request) {
+        return response('I\'m a teapot', Response::HTTP_I_AM_A_TEAPOT);
+    }
+}
