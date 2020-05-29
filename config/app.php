991841bced7bb2aa1f1afe52d373b3866f4694ff
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2020-05-29 11:42:13 +0100

    Recover needed classes to avoid deployment issues

--- config/app.php
+++ config/app.php
@@ -158,9 +158,9 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        //Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        //Illuminate\View\ViewServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
 
         /*
          * Package Service Providers...
