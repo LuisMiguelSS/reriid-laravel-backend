d222da4b644a2d9ae931ddb66864282a85860772
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2020-06-04 17:02:04 +0100

    Improved throttling measures

--- routes/api/internal.php
+++ routes/api/internal.php
@@ -13,25 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/', 'Route\RouteController@hello')->name('internal.alive');
+// API Throttling measures
+Route::group(['middleware' => 'api'], function () {
+    Route::any('/', 'Route\RouteController@hello')->name('internal.alive');
 
-// Email Verification
-Auth::routes(['register' => false]);
+    // Email Verification
+    Auth::routes(['register' => false]);
 
-// User token aunthentication NOT required
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('register', 'Auth\AuthController@register')->name('register');
-    Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
-    Route::post('login', 'Auth\AuthController@login')->name('login'); // This also needs email verification but it's implemented inside the login function
-});
+    // User token aunthentication NOT required
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('register', 'Auth\AuthController@register')->name('register');
+        Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+        Route::post('login', 'Auth\AuthController@login')->name('login'); // This also needs email verification but it's implemented inside the login function
+    });
 
-// User token authentication required
-Route::group([
-    'middleware' => [
-        'auth:api',
-        'email.verified'
-    ]
-], function () {
+
+    // User token authentication required
+    Route::group([
+        'middleware' => [
+            'auth:api',
+            'email.verified'
+        ]
+    ], function () {
 
     // Users
     Route::group(['prefix' => 'users'], function () {
@@ -67,4 +70,16 @@ Route::group([
         Route::post('{id}/restore', 'PostController@restore')->name('post.restore');
     });
 
+});
+
+});
+
+// Custom throttling measures
+Route::group([
+    'middleware' => 'throttle:300,1',
+    'prefix' => 'check'
+], function () {
+    Route::post('username/{username}', 'Auth\FieldController@usernameExists')->name('username.exists');
+    Route::post('email/{email}', 'Auth\FieldController@emailExists')->name('email.exists');
+    Route::post('user/{emailOrUsername}', 'Auth\FieldController@userExists')->name('user.exists');
 });
