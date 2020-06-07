932059926eaa9849f099363115d9997f1a403ece
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2020-06-07 18:18:36 +0100

    Include a WebSocketServer for realtime chat

---
+++ app/Console/Commands/WebSocketServer.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Console\Commands;
+
+require 'vendor\autoload.php';
+
+use Illuminate\Console\Command;
+use Ratchet\Server\IoServer;
+use \Ratchet\Http\HttpServer;
+use \Ratchet\WebSocket\WsServer;
+use App\Http\Controllers\WebSocketController;
+
+class WebSocketServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'websocket:init';
+
+    /**
+     * The port the websocket will be listening on.
+     * 
+     * @var int
+     */
+    protected static $port = 5010;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command starts the websocket server that is used for realtime chatting.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(new WebSocketController())
+            ),
+            WebSocketServer::$port 
+       );
+       $server->run();
+    }
+}
---
+++ app/Http/Controllers/WebSocketController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DateTime;
+use Ratchet\ConnectionInterface;
+use Ratchet\WebSocket\MessageComponentInterface;
+
+class WebSocketController extends Controller implements MessageComponentInterface
+{
+    private $connections = [];
+
+    /**
+     * Called when a new connection is opened.
+     * @param  ConnectionInterface $conn The socket that just connected
+     * @throws \Exception
+     */
+    function onOpen(ConnectionInterface $conn){
+        $this->connections[$conn->resourceId] = compact('conn') + ['username' => null];
+    }
+    
+    /**
+     * Called when a client sends data through the socket
+     * @param  \Ratchet\ConnectionInterface $conn The socket
+     * @param  string $msg The message received
+     * @throws \Exception
+     */
+    function onMessage(ConnectionInterface $from, $msg){
+
+        $message = json_decode($msg, true);
+
+        if (!isset($message['action']) || !isset($message['data'])) {
+            $this->sendMalformedJSONNotification($from);
+            return;
+
+        } else {
+
+            // Has action
+            switch ($message['action']) {
+                case 'set':
+                    // Set Username
+                    if (!isset($message['data']['username'])) {
+                        $this->sendMalformedJSONNotification($from);
+                        return;
+                    }
+
+                    // Check if it already exists
+                    foreach($this->connections as $resourceId => $connection) {
+                        if (isset($connection['username'])) {
+                            if ($connection['username'] == $message['data']['username']) {
+                                $from->send(json_encode(['error' => 'Username already in use']));
+                                return;
+                            }
+                        }
+
+                    }
+                    
+                    $this->connections[$from->resourceId]['username'] = $message['data']['username'];
+                    $from->send(json_encode(['Username successfully set.']));
+
+                break;
+                case 'send-message':
+
+                    // Check user has username
+                    if (!$this->hasUsername($from)) {
+                        $from->send(json_encode(['error' => 'An username must be set.']));
+                        return;
+                    }
+
+                    // Send message to user
+                    if (!isset($message['data']['message']) || !isset($message['data']['to'])) {
+                        $this->sendMalformedJSONNotification($from);
+                        return;
+                    }
+                    
+                    // Check target is not same as sender
+                    if ($message['data']['to'] == $this->getUsername($from)) {
+                        $from->send(json_encode(['error' => 'You cannot send a message to yourself.']));
+                    }
+
+                    // Search user
+                    foreach($this->connections as $resourceId => &$connection) {
+
+                        if (isset($connection['username'])) {
+                            if ($connection['username'] == $message['data']['to']) {
+                                $connection['conn']->send(json_encode([
+                                    'from' => $this->getUsername($from),
+                                    'message' => $message['data']['message'],
+                                    'timestamp' => (new DateTime())->format('c')
+                                ]));
+                                return;
+                            }
+                        }
+
+                    }
+                    $from->send(json_encode(['error' =>'User is not available.']));
+            }
+        }
+    }
+    
+     /**
+     * Called before or after a socket is closed (depends on how it's closed). 
+     * SendMessage to $conn will not result in an error if it has already been closed.
+     * 
+     * @param  ConnectionInterface $conn The socket/connection that is closing/closed
+     * @throws \Exception
+     */
+    function onClose(ConnectionInterface $conn){
+        unset($this->connections[$conn->resourceId]);
+    }
+    
+     /**
+     * If there is an error with one of the sockets, or somewhere in the application where an Exception is thrown,
+     * the Exception is sent back down the stack, handled by the Server and bubbled back up the application through this method
+     * @param  ConnectionInterface $conn
+     * @param  \Exception $e
+     * @throws \Exception
+     */
+    function onError(ConnectionInterface $conn, \Exception $e){
+        echo "An error occurred with connection $conn->resourceId: " .$e->getMessage()." at line ".$e->getLine();
+
+        unset($this->connections[$conn->resourceId]);
+
+        $conn->close();
+    }
+
+    function sendMalformedJSONNotification(ConnectionInterface $connection) {
+        $connection->send(json_encode([
+            'error' => 'Malformed JSON: No correct action or data indicated.'
+        ]));
+    }
+
+    function hasUsername(ConnectionInterface $connection) {
+        return isset($this->connections[$connection->resourceId]['username']);
+    }
+
+    function getUsername(ConnectionInterface $connection) {
+        return $this->hasUsername($connection)
+                ?
+                $this->connections[$connection->resourceId]['username']
+                :
+                null;
+    }
+    
+}
